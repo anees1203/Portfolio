42b0322bccde7b6b9f2535dc54a8ea63b7c99b17
Anees Shaik <anees.shaik@slu.edu>
2023-12-23 15:40:12 -0600

    upgraded 2 home

--- src/components/InteractiveSphere.tsx
+++ src/components/InteractiveSphere.tsx
@@ -1,69 +1,89 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 
-interface InteractiveSphereProps {
-  color?: string;
-}
-
-const InteractiveSphere: React.FC<InteractiveSphereProps> = ({ color = '#00fffc' }) => {
-  const mountRef = useRef<HTMLDivElement>(null);
+const InteractiveNetworkSphere = ({ color = '#00fffc' }) => {
+  const mountRef = useRef(null);
 
   useEffect(() => {
     if (!mountRef.current) return;
 
-    // Use the dimensions of the mountRef element
-    const { clientWidth: width, clientHeight: height } = mountRef.current;
-
+    const width = mountRef.current.clientWidth;
+    const height = mountRef.current.clientHeight;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ alpha: true }); // Ensure the background is transparent
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(width, height);
-    mountRef.current.appendChild(renderer.domElement); // Append to the mountRef element
+    mountRef.current.appendChild(renderer.domElement);
 
-    // Create a sphere
-    const sphereGeometry = new THREE.SphereGeometry(5, 20, 20);
-    const sphereMaterial = new THREE.PointsMaterial({ color: color, size: 0.1 });
-    const sphere = new THREE.Points(sphereGeometry, sphereMaterial);
-    scene.add(sphere);
+    // Generate random nodes on the sphere's surface
+    const nodePositions = Array.from({ length: 100 }, () => {
+      const u = Math.random();
+      const v = Math.random();
+      const theta = u * 2.0 * Math.PI;
+      const phi = Math.acos(2.0 * v - 1.0);
+      const r = 7; // radius of the sphere
+      const x = r * Math.sin(phi) * Math.cos(theta);
+      const y = r * Math.sin(phi) * Math.sin(theta);
+      const z = r * Math.cos(phi);
+      return [x, y, z];
+    });
 
-    // Set camera position
-    camera.position.y = -2; // Adjust this value to move the sphere up
-    camera.position.z = 12;
+    // Create random connections between nodes
+    const connections = Array.from({ length: 50 }, () => [
+      Math.floor(Math.random() * nodePositions.length),
+      Math.floor(Math.random() * nodePositions.length),
+    ]);
 
-    // Animation loop
+    // Create nodes
+    const nodesGeometry = new THREE.BufferGeometry();
+    const nodesMaterial = new THREE.PointsMaterial({ color: color, size: 0.12 });
+    const vertices = new Float32Array(nodePositions.flat());
+    nodesGeometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    const nodes = new THREE.Points(nodesGeometry, nodesMaterial);
+    scene.add(nodes);
+
+    // Create edges
+    const edgesGeometry = new THREE.BufferGeometry();
+    const edgesMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
+    const edgeVertices = [];
+    connections.forEach(([startIndex, endIndex]) => {
+      edgeVertices.push(...nodePositions[startIndex], ...nodePositions[endIndex]);
+    });
+    edgesGeometry.setFromPoints(new THREE.Float32BufferAttribute(edgeVertices, 3));
+    const lineSegments = new THREE.LineSegments(edgesGeometry, edgesMaterial);
+    scene.add(lineSegments);
+
+    camera.position.z = 15;
+
     const animate = () => {
       requestAnimationFrame(animate);
-      sphere.rotation.y += 0.0009; // Slow continuous rotation
+      nodes.rotation.y += 0.005;
+      lineSegments.rotation.y += 0.005;
       renderer.render(scene, camera);
     };
     animate();
 
-    // Handle mouse movement
-    const handleMouseMove = (event: MouseEvent) => {
+    const handleMouseMove = (event) => {
       const rect = renderer.domElement.getBoundingClientRect();
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
       const xRotation = (y / height - 0.5) * 2;
       const yRotation = (x / width - 0.5) * 2;
-      sphere.rotation.x = xRotation;
-      sphere.rotation.y = yRotation;
+      nodes.rotation.x = xRotation;
+      nodes.rotation.y = yRotation;
+      lineSegments.rotation.x = xRotation;
+      lineSegments.rotation.y = yRotation;
     };
 
-    // Add mousemove event listener
     renderer.domElement.addEventListener('mousemove', handleMouseMove);
 
-    // Cleanup on unmount
     return () => {
       renderer.domElement.removeEventListener('mousemove', handleMouseMove);
       mountRef.current.removeChild(renderer.domElement);
     };
   }, [color]);
 
-  return (
-    <div ref={mountRef} style={{ width: '100%', height: '100vh' }}>
-      {/* The Three.js canvas will be appended here */}
-    </div>
-  );
+  return <div ref={mountRef} style={{ width: '100%', height: '100vh' }} />;
 };
 
-export default InteractiveSphere;
+export default InteractiveNetworkSphere;
