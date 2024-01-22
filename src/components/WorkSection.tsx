b579f127ad3b60e3ce19e2fe5817f909bd267dfd
Anees Shaik <anees.shaik@slu.edu>
2024-01-22 16:37:51 -0600

    done with worksection

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import YourImage from "../assets/Images/project1.png";
 
 function WorkSection() {
   return (
-    <div className="work-section">WorkSection</div>
+    <div className="work-section">
+      <h1 className="work-heading">{'< P R O J E C T S >'}</h1>
+      <p className="tagline">Here you will find some of my personal projects.</p>
+
+      <div className="content-container">
+        <div className="image1-container">
+          <img src={YourImage} alt="Your Image" />
+        </div>
+        <div className="text-section">
+          <h2 className="text-heading">{'Diabetes Diet Hub'}</h2>
+          <p className="text">DDH is a project I developed, focused on providing personalized health management tools. It's built to empower users to confidently manage their well-being with the support of a community, marking my commitment to innovative health solutions.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
