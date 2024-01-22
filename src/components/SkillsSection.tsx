39e1a772ffcb9a36c8e1301fbad47b6ea647cf8f
Anees Shaik <anees.shaik@slu.edu>
2024-01-22 15:13:46 -0600

    completed the skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
+import python from "../assets/Images/python.png";
+import java from "../assets/Images/java.png";
+import react from "../assets/Images/react.png";
+import typescript from "../assets/Images/typescript.png";
+import javascript from "../assets/Images/javascript.png";
+import bootstrap from "../assets/Images/bootstrap.png";
+import css from "../assets/Images/css-3.png";
+import cplus from "../assets/Images/c-.png";
+import HTML from "../assets/Images/HTML.png";
+import github from "../assets/Images/github.png";
+import SQL from "../assets/Images/database-storage.png";
+import circleci from "../assets/Images/circleci.png";
+
+import YourImage from "../assets/Images/computer.png"; // Replace 'your-image.jpg' with your image file path
 
 function SkillSection() {
+  const icons = [
+    { src: python, label: 'Python' },
+    { src: java, label: 'Java' },
+    { src: cplus, label: 'c++'},
+    { src: css, label: 'css'},
+    { src: HTML, label: 'HTML'},
+    { src: react, label: 'react' },
+    { src: typescript, label: 'typescript'},
+    { src: javascript, label: 'javascript'},
+    { src: bootstrap, label: 'bootstrap'},
+    { src: github, label: 'github'},
+    { src: SQL, label: 'SQL'},
+    { src: circleci, label: 'circle CI'},
+
+    // Add more icons as needed
+  ];
+
   return (
-    <div className="skills-section">skills</div>
+    <div className="skills-section">
+      {/* Centered Heading */}
+      <h1 className="skills-heading">{'< My Tech Stack >'}</h1>
+
+      {/* Parent container for Icons Section and Image Container */}
+      <div className="content-container">
+
+        {/* Image Section */}
+        <div className="image-container">
+          <img src={YourImage} alt="Your Image" />
+          <p className="developer-tagline">Engineer by day, Creator by night; Turning coffee into code.</p>
+        </div>
+
+        {/* Icons Section */}
+        <div className="icons-section">
+          <div className="icons-grid">
+            {icons.map((icon, index) => (
+              <div key={index} className="icon">
+                <img src={icon.src} alt={icon.label} />
+                <p>{icon.label}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
